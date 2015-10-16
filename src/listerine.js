4968825ed027357b65bd1d3febff319ad1238715
Terrence Howard <chemisus@gmail.com>
2015-10-15 21:47:24 -0400

    restart

--- src/listerine.js
+++ src/listerine.js
@@ -1,118 +1,128 @@
-Array.prototype.mapFilter = function (map, filter) {
+function mapFilter(map, filter) {
     return this.filter(function (value, key) {
         return filter(map(value, key), key);
     });
-};
+}
 
-Array.prototype.mapSort = function (map, sort) {
+function mapSort(map, sort) {
     return this.sort(function (a, b) {
         return sort(map(a), map(b));
     });
+}
+
+function rename(callback) {
+    var object = {};
+    var keys = [];
+    this.forEach(function (value, key) {
+        key = callback(value, key);
+        object[key] = value;
+        keys.push(key);
+    });
+    return Object.with(object, keys);
+}
+
+function group(callback) {
+    var groups = Object.with();
+    this.forEach(function (value, key) {
+        var group = callback(value, key);
+        if (!groups.has(group)) {
+            groups.set(group, Object.with());
+        }
+        groups.get(group).set(key, value);
+    });
+    return groups;
+}
+
+Array.prototype.has = function (key) {
+    return key <= this.length;
 };
 
-function List(keys, values) {
-    this.keys = keys;
-    this.values = values;
+Array.prototype.get = function (key) {
+    return this[key];
+};
+
+Array.prototype.set = function (key, value) {
+    return this[key] = value;
+};
+
+Array.prototype.delete = function (key) {
+    this.splice(key, 1);
+};
+
+Array.prototype.mapFilter = mapFilter;
+Array.prototype.mapSort = mapSort;
+Array.prototype.group = group;
+Array.prototype.rename = rename;
+
+function Map(object, keys) {
+    this.object = object || {};
+    this.keys = keys || Object.keys(object);
 }
-List.prototype = {
-    /**
-     *
-     * @param callback
-     * @returns {List}
-     */
+Map.prototype = {
+    keys: [],
+    object: {},
+    get values() {
+        return this.keys.map(function (key) {
+            return this.get(key);
+        }.bind(this));
+    },
+    has: function (key) {
+        return this.object.hasOwnProperty(key);
+    },
+    get: function (key) {
+        return this.object[key];
+    },
+    set: function (key, value) {
+        return this.object[key] = value;
+    },
+    delete: function (key) {
+        delete this.object[key];
+    },
     forEach: function (callback) {
-        this.keys.map(function (key) {
-            if (this.values.hasOwnProperty(key)) {
-                callback(this.values[key], key);
-            }
+        this.keys.forEach(function (key) {
+            callback(this.get(key), key);
         }.bind(this));
         return this;
     },
-
-    /**
-     *
-     * @param callback
-     * @returns {List}
-     */
     map: function (callback) {
-        var values = {};
+        var object = {};
         this.forEach(function (value, key) {
-            values[key] = callback(value, key);
+            object[key] = callback(value, key);
         });
-        return new List(this.keys, values);
+        return Object.with(object, this.keys);
     },
-
-    /**
-     *
-     * @param callback
-     * @param initial
-     * @returns {*}
-     */
+    filter: function () {
+        var object = {};
+        this.forEach(function (value, key) {
+            if (callback(value, key)) {
+                object[key] = value;
+            }
+        });
+        return Object.with(object, this.keys);
+    },
     reduce: function (callback, initial) {
-        return this.keys.reduce(function (a, b) {
-            return callback(a, this.values[b]);
-        }.bind(this), initial);
+        this.forEach(function (value, key) {
+            initial = callback(initial, value, key);
+        });
+        return initial;
     },
-
-    /**
-     *
-     * @param map
-     * @param filter
-     * @returns {List}
-     */
-    mapFilter: function (map, filter) {
-        var values = {};
-        var keys = [];
-
-        this.keys
-            .mapFilter(function (key) {
-                return map(this.values[key], key);
-            }.bind(this), filter)
-            .forEach(function (key) {
-                keys.push(key);
-                values[key] = this.values[key];
-            }.bind(this));
-
-        return new List(keys, values);
+    sort: function (callback) {
+        var keys = this.keys.sort(function (a, b) {
+            return callback(a, b);
+        });
+        return Object.with(this.object, keys);
     },
-
-    /**
-     *
-     * @param map
-     * @param sort
-     * @returns {List}
-     */
-    mapSort: function (map, sort) {
-        var keys = this.keys.mapSort(function (key) {
-            return map(this.values[key], key);
-        }.bind(this), sort);
-        return new List(this.keys, this.values);
-    }
+    mapFilter: mapFilter,
+    mapSort: mapSort,
+    rename: rename,
+    group: group
 };
 
-/**
- *
- * @param object
- * @returns {List}
- */
-Object.with = function (object) {
-    return new List(Object.keys(object), object);
-};
-
-/**
- *
- * @param keys
- * @param values
- * @returns {List}
- */
-Object.combine = function (keys, values) {
-    var object = {};
-    for (var i = 0; i < keys.length; i++) {
-        object[keys[i]] = values[i];
-    }
-    return new List(keys, object);
+Object.with = function (object, keys) {
+    object = object || {};
+    return new Map(object, keys || Object.keys(object));
 };
 
 module.exports = {
-    List: List
+    Map: Map
 };
